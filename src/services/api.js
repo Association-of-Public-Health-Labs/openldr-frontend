3216bb1f448764fcadfe9ed527d6ca1706078305
Vagner Pene <vagnerpene@gmail.com>
2020-04-06 16:14:35 +0200

    Update the api URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://openldr.herokuapp.com"
-  baseURL: "http://localhost:4444"
+  baseURL: "https://openldr.herokuapp.com"
+  // baseURL: "http://localhost:4444"
 });
 
 export default api;
