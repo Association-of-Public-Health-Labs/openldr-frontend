c5a90ca690dc88db9e0eeca51bfff95c59ddcd9f
Vagner Pene <vagnerpene@gmail.com>
2020-04-07 00:31:24 +0200

    Linking Dashboard page with the api

--- src/cards/dashboard/Overview/index.js
+++ src/cards/dashboard/Overview/index.js
@@ -1,6 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import { ThemeContext } from "styled-components";
+import qs from "qs";
+import moment from "moment";
+import average from "average";
+import api from "../../../services/api";
 
 import OverviewChart from "../../../components/Charts/OverviewChart";
 
@@ -10,8 +14,108 @@ export default function Overview() {
   const classes = useStyles();
   const { colors } = useContext(ThemeContext);
 
-  const labels = ["12pm", "3pm", "6pm", "9pm", "12am", "3am", "6am", "9am"];
-  const data = [40, 500, 650, 700, 1200, 1250, 1300, 1900];
+  const [numberOfSamples, setNumberOfSamples] = useState(0);
+  const [samplesLabels, setSamplesLabels] = useState([]);
+  const [samplesData, setSamplesData] = useState([]);
+  const [suppLabels, setSuppLabels] = useState([]);
+  const [suppData, setSuppData] = useState([]);
+  const [tatLabels, setTatLabels] = useState([]);
+  const [tatData, setTatData] = useState([]);
+  const [dates, setDates] = useState([
+    moment()
+      .subtract(1, "year")
+      .format("YYYY-MM-DD"),
+    moment().format("YYYY-MM-DD")
+  ]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get("/dash_number_of_samples", {
+        params: {
+          dates: dates
+        },
+        paramsSerializer: params => {
+          return qs.stringify(params);
+        }
+      });
+      const results = response.data;
+      var labels = [],
+        data = [];
+
+      results.map(result => {
+        labels.push(result.month);
+        data.push(result.total);
+        setNumberOfSamples(prevState => prevState + result.total);
+      });
+      setSamplesLabels(labels);
+      setSamplesData(data);
+    }
+    loadData();
+  }, [dates]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get("/dash_viral_suppression", {
+        params: {
+          dates: dates
+        },
+        paramsSerializer: params => {
+          return qs.stringify(params);
+        }
+      });
+      const results = response.data;
+      var labels = [],
+        data = [];
+
+      results.map(result => {
+        labels.push(result.month);
+        const { suppressed, total } = result;
+        if (total > 0) {
+          data.push(Math.round((suppressed / total) * 100));
+        } else {
+          data.push(0);
+        }
+      });
+      setSuppLabels(labels);
+      setSuppData(data);
+    }
+    loadData();
+  }, [dates]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get("/dash_tat", {
+        params: {
+          dates: dates
+        },
+        paramsSerializer: params => {
+          return qs.stringify(params);
+        }
+      });
+      const results = response.data;
+      var labels = [],
+        data = [];
+
+      results.map(result => {
+        labels.push(result.month);
+        const {
+          collection_reception,
+          reception_registration,
+          registration_analysis,
+          analysis_validation
+        } = result;
+        data.push(
+          collection_reception +
+            reception_registration +
+            registration_analysis +
+            analysis_validation
+        );
+      });
+      setTatLabels(labels);
+      setTatData(data);
+    }
+    loadData();
+  }, [dates]);
 
   return (
     <Container>
@@ -19,13 +123,13 @@ export default function Overview() {
         <Grid item xs={6} sm={4} md={4} lg={4}>
           <Card>
             <CardText>
-              <h5>Number of Samples</h5>
-              <h1>5690000</h1>
+              <h5>Numero de amostras</h5>
+              <h1>{numberOfSamples}</h1>
             </CardText>
             <ChartCanvas>
               <OverviewChart
-                labels={labels}
-                dataset={data}
+                labels={samplesLabels}
+                dataset={samplesData}
                 color={colors.primary}
               />
             </ChartCanvas>
@@ -34,24 +138,28 @@ export default function Overview() {
         <Grid item xs={6} sm={4} md={4} lg={4}>
           <Card>
             <CardText>
-              <h5>Viral Suppression</h5>
-              <h1>72%</h1>
+              <h5>Supressao Viral</h5>
+              <h1>{Math.round(average(suppData))}%</h1>
             </CardText>
             <ChartCanvas>
-              <OverviewChart labels={labels} dataset={data} color="#ef5350" />
+              <OverviewChart
+                labels={suppLabels}
+                dataset={suppData}
+                color="#ef5350"
+              />
             </ChartCanvas>
           </Card>
         </Grid>
         <Grid item xs={6} sm={4} md={4} lg={4}>
           <Card>
             <CardText>
-              <h5>Turn around time</h5>
-              <h1>21 days</h1>
+              <h5>Tempo de Resposta</h5>
+              <h1>{Math.round(average(tatData))} dias</h1>
             </CardText>
             <ChartCanvas>
               <OverviewChart
-                labels={labels}
-                dataset={data}
+                labels={tatLabels}
+                dataset={tatData}
                 color={colors.tertiary}
               />
             </ChartCanvas>
