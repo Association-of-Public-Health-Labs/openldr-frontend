ab354737a80c349ae694e58bc2972ecc527f64d9
Vagner Pene <vagnerpene@gmail.com>
2022-12-19 15:06:11 +0200

    update the base URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://openldr.herokuapp.com",
-  baseURL: "http://queue.openldr.org.mz:5555",
+  baseURL: "https://queue.openldr.org.mz",
   // baseURL: "http://localhost:4444",
 });
 
