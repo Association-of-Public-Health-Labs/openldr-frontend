64db651d4133a17083e6055071a06f4d7072c3b6
Vagner Pene <vagnerpene@gmail.com>
2022-12-19 10:32:43 +0200

    change htmlToImage to toPng in utils/htmlToImage

--- src/utils/htmlToImage.js
+++ src/utils/htmlToImage.js
@@ -1,9 +1,9 @@
-import htmlToImage from "html-to-image";
+import {toPng} from "html-to-image";
 import download from "downloadjs";
 
 function convertCardToImage(cardId) {
   if (cardId) {
-    htmlToImage.toPng(document.getElementById(cardId)).then(function(dataUrl) {
+    toPng(document.getElementById(cardId)).then(function(dataUrl) {
       download(dataUrl, cardId + ".png");
     });
   }
