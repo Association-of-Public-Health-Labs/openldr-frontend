250e7b4756c1b4c6085a72b2e304f1c358b8d918
Vagner Pene <vagnerpene@gmail.com>
2023-02-01 15:52:46 +0200

    update backend api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://openldr.herokuapp.com",
-  baseURL: "https://queue.openldr.org.mz",
+  baseURL: "https://aphl.openldr.org.mz",
   // baseURL: "http://localhost:4444",
 });
 
