15ab66182b11802d16995cf2c73d66cac12208f6
Vagner Pene <vagnerpene@gmail.com>
2023-03-06 09:50:47 +0200

    update backend url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://openldr.herokuapp.com",
-  baseURL: "https://aphl.openldr.org.mz",
+  baseURL: "https://openldr-backend.onrender.com",
   // baseURL: "http://localhost:4444",
 });
 
