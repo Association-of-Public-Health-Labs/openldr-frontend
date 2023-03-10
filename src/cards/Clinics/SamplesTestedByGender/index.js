7242fedb837354ccd04516854007ce49b2415403
Vagner Pene <vagnerpene@gmail.com>
2023-03-10 12:06:46 +0200

    Add feature to extract raw data to excel

--- src/cards/Clinics/SamplesTestedByGender/index.js
+++ src/cards/Clinics/SamplesTestedByGender/index.js
@@ -3,11 +3,12 @@ import hexToRgba from "hex-to-rgba";
 import moment from "moment";
 import qs from "qs";
 import api from "../../../services/api";
-// import Card from "../../../components/MainCard";
 
 import Card from "../../../components/MasterCard";
 import BarGroup from "../../../components/Charts/BarGroup";
-import PatientsListPopup from "../../../components/PatientsListPopup";
+
+import exportToExcel from "../../../utils/exportToExcel";
+import excelConfig from "../../../config/excel";
 
 const startDate = moment().subtract(1, "year").format("YYYY-MM-DD");
 const endDate = moment().format("YYYY-MM-DD");
@@ -25,9 +26,6 @@ export default function SamplesTestedByGender() {
   const [disaggregation, setDisaggregation] = useState(false);
   const [dates, setDates] = useState([startDate, endDate]);
   const [isLoading, setIsLoading] = useState(true);
-  const [location, setLocation] = useState(null)
-  const [visible, setVisible] = useState(false)
-  const [sqlQuery, setSQLQuery] = useState(null)
 
   useEffect(() => {
     async function loadData() {
@@ -46,59 +44,11 @@ export default function SamplesTestedByGender() {
         }
       );
       const results = response.data;
+      setChartData(results)
       setIsLoading(false);
-      var chartLabels = [],
-        male_suppressed = [],
-        female_suppressed = [],
-        male_not_suppressed = [],
-        female_not_suppressed = [];
-
-      results.map((result) => {
-        chartLabels.push(result.facility);
-        male_suppressed.push(result.male_suppressed);
-        female_suppressed.push(result.female_suppressed);
-        male_not_suppressed.push(result.male_not_suppressed);
-        female_not_suppressed.push(result.female_not_suppressed);
-        setChartType(result.type)
-      });
-
-      setLabels(chartLabels);
-      setData([
-        {
-          label: "Homens (CV < 1000)",
-          backgroundColor: "#00b000",
-          stack: "Stack 0",
-          data: male_suppressed,
-        },
-        {
-          label: "Homens (CV > 1000)",
-          backgroundColor: hexToRgba("#00b000", "0.4"),
-          stack: "Stack 0",
-          data: male_not_suppressed,
-        },
-        {
-          label: "Mulheres (CV < 1000)",
-          backgroundColor: "#e74c3c",
-          stack: "Stack 1",
-          data: female_suppressed,
-        },
-        {
-          label: "Mulheres (CV > 1000)",
-          backgroundColor: hexToRgba("#e74c3c", "0.4"),
-          stack: "Stack 1",
-          data: female_not_suppressed,
-        },
-      ]);
-      setLabelsExcel(["", ...chartLabels]);
-      setDataExcel([
-        ["Homens (CV < 1000)", ...male_suppressed],
-        ["Homens (CV > 1000)", ...male_not_suppressed],
-        ["Mulheres (CV < 1000)", ...female_suppressed],
-        ["Mulheres (CV > 1000)", ...female_not_suppressed],
-      ]);
     }
     loadData();
-  }, [facilities, dates]);
+  }, [facilities, dates, type, disaggregation]);
 
   const handleGetParams = (param) => {
     if (param.facilityType === "province") {
@@ -118,32 +68,95 @@ export default function SamplesTestedByGender() {
     setIsLoading(true);
   };
 
-  const handleChartClick = (value) => {
-    setDisaggregation(true);
-    if (chartType === "province") {
-      setFacilities([value]);
-      setDisaggregation(true);
-      setType("district");
-      setIsLoading(true);
-    } else if (chartType === "district") {
-      setFacilities([value]);
-      setDisaggregation(true);
-      setType("clinic");
-      setIsLoading(true);
-    }else if (chartType === "clinic") {
-      setSQLQuery(`AnalysisDatetime >= '${dates[0]}' AND AnalysisDatetime <= '${dates[1]}' AND RequestingFacilityName='${value}' AND ViralLoadResultCategory IS NOT NULL`)
-      setLocation(value)
-      setVisible(true)
+  const handleChartClick = async (value) => {
+    setIsLoading(true)
+    if (chartType === "clinic") {
+      await exportRawData(value)
+      setIsLoading(false)
+      return
     }
+    const response = await api.get("/clinic_samples_tested_by_gender_and_facility", {
+      params: {
+        codes: [value],
+        dates: dates,
+        type: chartType,
+        disaggregation: true,
+      },
+      paramsSerializer: (params) => {
+        return qs.stringify(params);
+      },
+    });
+    const results = response.data;
+    setChartData(results);
+    setChartType(chartType => chartType === "district" ? "district" : (chartType === "clinic" ? "clinic" : "province"))
+    setIsLoading(false)
   };
 
-  const handleClosePopup = (value) => {
-    setVisible(value)
+  async function exportRawData (healthFacility) {
+    const jwt_token = localStorage.getItem("@RAuth:token");
+    const query = `AnalysisDatetime >= '${dates[0]}' AND AnalysisDatetime <= '${dates[1]}' AND RequestingFacilityName='${healthFacility}' AND ViralLoadResultCategory IS NOT NULL`
+    const response = await api.get("/viralload/all_patients/query/" + query, {
+      headers: {
+          authorization: `Bearer ${jwt_token}`,
+      },
+    });
+    await exportToExcel(healthFacility, healthFacility, excelConfig?.headers, response.data);
+    setIsLoading(false)
+  }
+
+  function setChartData(results) {
+    var chartLabels = [],
+        male_suppressed = [],
+        female_suppressed = [],
+        male_not_suppressed = [],
+        female_not_suppressed = [];
+
+    results.map((result) => {
+      chartLabels.push(result.facility);
+      male_suppressed.push(result.male_suppressed);
+      female_suppressed.push(result.female_suppressed);
+      male_not_suppressed.push(result.male_not_suppressed);
+      female_not_suppressed.push(result.female_not_suppressed);
+      setChartType(result.type)
+    });
+
+    setLabels(chartLabels);
+    setData([
+      {
+        label: "Homens (CV < 1000)",
+        backgroundColor: "#00b000",
+        stack: "Stack 0",
+        data: male_suppressed,
+      },
+      {
+        label: "Homens (CV > 1000)",
+        backgroundColor: hexToRgba("#00b000", "0.4"),
+        stack: "Stack 0",
+        data: male_not_suppressed,
+      },
+      {
+        label: "Mulheres (CV < 1000)",
+        backgroundColor: "#e74c3c",
+        stack: "Stack 1",
+        data: female_suppressed,
+      },
+      {
+        label: "Mulheres (CV > 1000)",
+        backgroundColor: hexToRgba("#e74c3c", "0.4"),
+        stack: "Stack 1",
+        data: female_not_suppressed,
+      },
+    ]);
+    setLabelsExcel(["", ...chartLabels]);
+    setDataExcel([
+      ["Homens (CV < 1000)", ...male_suppressed],
+      ["Homens (CV > 1000)", ...male_not_suppressed],
+      ["Mulheres (CV < 1000)", ...female_suppressed],
+      ["Mulheres (CV > 1000)", ...female_not_suppressed],
+    ]);
   }
 
   return (
-    <>
-    {visible && <PatientsListPopup location={location} dates={dates} query={sqlQuery} handleClosePopup={handleClosePopup}/>}
     <Card
       cardId={cardId}
       cardTitle={cardTitle}
@@ -165,6 +178,5 @@ export default function SamplesTestedByGender() {
     >
       <BarGroup datasets={data} labels={labels} onClick={handleChartClick} />
     </Card>
-    </>
   );
 }
